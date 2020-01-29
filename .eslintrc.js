3fcfa6d41ca5ad708e5c7eed237b33ad77591901
Marco Dizo <mdizo6482@gmail.com>
2020-01-29 09:17:23 +0800

    Updated Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  parser: "vue-eslint-parser",
-  extends: [
-    'plugin:gridsome/recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: 'babel-eslint'
-  }
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es6: true
+  },
+  plugins: ['gridsome'],
+  extends: ['prettier', 'plugin:gridsome/recommended']
 }
